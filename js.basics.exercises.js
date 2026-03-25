75cf9fea167b411346d305790456fb11e807d6dc
chandana41125 <chandanachandu41125@gmail.com>
2026-03-25 13:12:11 +0530

    Added JavaScript basics exercises

---
+++ js.basics.exercises.js
@@ -0,0 +1,65 @@
+// JavaScript Basics Practice - Lesson 1
+// Author: Chandana
+
+// -----------------------------------
+// 1a. Use alert(...); to display 'Good morning!' in a popup
+// -----------------------------------
+alert('Good morning!');
+
+
+// -----------------------------------
+// 1b. Display your name in a popup
+// -----------------------------------
+alert('Chandana');
+
+
+// -----------------------------------
+// 1c. Using math, calculate 10 + 5 in the Console
+// -----------------------------------
+console.log(10 + 5); // Output: 15
+
+
+// -----------------------------------
+// 1d. Calculate 20 - 5 in the Console
+// -----------------------------------
+console.log(20 - 5); // Output: 15
+
+
+// -----------------------------------
+// 1e. Calculate 2 + 2 - 5 in the Console
+// -----------------------------------
+console.log(2 + 2 - 5); // Output: -1
+
+
+// -----------------------------------
+// 1f. Use document.body.innerHTML to display 'Good morning!' on the web page
+// -----------------------------------
+document.body.innerHTML = 'Good morning!';
+
+
+// -----------------------------------
+// 1g. Display your name on the web page
+// -----------------------------------
+// (Note: This will replace previous content)
+document.body.innerHTML = 'Chandana';
+
+
+// -----------------------------------
+// 1h. You order a T-shirt for $10, socks for $8, and dinner plates for $20.
+// Use JavaScript to calculate the total cost
+// -----------------------------------
+     10+8+20 // Output: 38
+
+
+// -----------------------------------
+// 1i. Your bank account has $100, you spend $20 on lunch,
+// $50 on dinner, and earn $200 from your job.
+// Calculate how much money you have
+// -----------------------------------
+    100-20-50-200 // Output: 230
+
+
+// -----------------------------------
+// 1j. Use document.body.innerHTML to make the web page blank
+// -----------------------------------
+document.body.innerHTML = '';
